5f05bf3ac630281e3613fb440e9fe4465cafab12
Talha Jobayer <talhajobayer@gmail.com>
2022-03-25 22:14:34 +0600

    person card complete

--- src/Component/Person/Person.js
+++ src/Component/Person/Person.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import './Person.css'
 
 const Person = (props) => {
-         const {gender}=props.person
+         const {gender,first_name,email,expected_salery,image}=props.person
     return (
-        <div>
-            <h1>gender:{gender}</h1>
+        <div className='person-container'>
+            <div className="image-part">
+                <img src={image} alt="" />
+            </div>
+            <div className='text-part'>
+            <h1><b>Name:</b> {first_name} </h1>
+            <p><b>Email:</b> {email}</p>
+            <p><b>Expected salery: $</b>{expected_salery}</p>
+            <p><b>Gender</b>: {gender}</p>
+            
+            </div>
+            <button className='Hire-Me'>Hire Me</button>
         </div>
     );
 };
